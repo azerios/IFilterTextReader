938554108eb49a8089010ba7f0008cb550ff917f
azerios <93074569+azerios@users.noreply.github.com>
2026-05-05 13:45:37 +0200

    Update Constants.cs

--- IFilterTextReader.Tests/Helpers/Constants.cs
+++ IFilterTextReader.Tests/Helpers/Constants.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace IFilterTextReader.Tests.Helpers
+{
+    internal class Constants
+    {
+        /// <summary>
+        ///     Constants used across tests, such as test file paths and common strings
+        /// </summary>
+        internal const string TestFilesPath = "TestFiles";
+    }
+}
